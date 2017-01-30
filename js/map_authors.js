8a9671414f061d190f7921664a85767b2407b89f
Kevin Cazelles <kevin.cazelles@um2.fr>
2017-01-30 12:43:06 +0100

    Deployed 5397a6f with MkDocs version: 0.16.1

--- js/map_authors.js
+++ js/map_authors.js
@@ -1,6 +1,7 @@
-// Script to creat a map of collaborators
+// Script to creat a map of to locate ourselves
+// KC: 30/01/2017
+
 collabo = YAML.load("../data/authors_info.yml");
-document.getElementById('map_authors').innerHTML += "coll"
 //
 var mymapc = L.map('map_authors', {
     zoomControl: false
@@ -11,51 +12,24 @@ L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{
      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
      maxZoom: 20,
  }).addTo(mymapc);
-
 //
 var zoomHomec = new L.Control.zoomHome([47, -32], 3);
 zoomHomec.addTo(mymapc);
 
-//
-var myMarkerc = L.AwesomeMarkers.icon({
-  icon: 'user',
-  prefix: 'fa',
-  markerColor: 'darkblue'
-});
-//
-var myMarkerme = L.AwesomeMarkers.icon({
-  icon: 'user',
-  prefix: 'fa',
-  markerColor: 'darkred'
-});
 
+// NB: you cam use your own icon: http://leafletjs.com/examples/custom-icons/
 //
-function add_hyperlink(arg, arg2) {
-  var out;
-  if (arg == null) {
-    out = "";
+//
+for (var i=0; i<collabo.length; i++){
+  for (var j=0; j<collabo[i].institution.length; j++){
+    var lat=collabo[i].institution[j].lat;
+    var lon=collabo[i].institution[j].lon;
+     L.marker([lat,lon]).addTo(mymapc)
+     .bindPopup("<b>" + collabo[i].given_name + collabo[i].family_name + "</b>"
+     + "<br>" + collabo[i].institution[j].name + "<br>"
+     + add_hyperlink(collabo[i].institution[j].osmlink, "<i class='fa fa-expand' aria-hidden='true'></i> &nbsp;")
+     + add_hyperlink(collabo[i].website, " <i class='fa fa-link' aria-hidden='true'></i> &nbsp;")
+     + add_hyperlink(collabo[i].github, " <i class='fa fa-github fa'></i> &nbsp;")
+   );
   }
-  else {
-    out = "<a href=" + arg + ">" + arg2 + "<a/>";
-  }
-  return out;
 }
-
-// L.marker([48.452900, -68.512790], {icon: myMarkerme}).addTo(mymapc);
-
-//
-//
-//
-// for (var i=0; i<collabo.length; i++){
-//   for (var j=0; j<collabo[i].institution.length; j++){
-//     var lat=collabo[i].institution[j].lat;
-//     var lon=collabo[i].institution[j].lon;
-//      L.marker([lat,lon], {icon: myMarkerc}).addTo(mymapc)
-//      .bindPopup(collabo[i].given_name + " " + collabo[i].family_name
-//      + "<br>" + collabo[i].institution[j].name + "<br>"
-//      + add_hyperlink(collabo[i].institution[j].osmlink, "<i class='fa fa-globe fa'></i> &nbsp;")
-//      + add_hyperlink(collabo[i].website, " Website &nbsp;")
-//      + add_hyperlink(collabo[i].github, " <i class='fa fa-github fa'></i> &nbsp;")
-//    );
-//   }
-// }
